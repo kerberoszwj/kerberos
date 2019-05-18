9c60a3e87a7aa8e30e73a296fbd546508ff2f5de
yyz <773645319@qq.com>
2019-05-18 08:55:48 +0800

    改

--- v.py
+++ v.py
@@ -3,7 +3,7 @@ import rsa2
 from Common import *
 import socket
 import multiprocessing
-import threading
+import _thread
 
 def jieshou():
     client,addr=server.accept()
@@ -13,8 +13,8 @@ def jieshou():
         indata = (client.recv(2048)).decode('utf-8')
     except Exception as e:
             print("没收到")
-
     return indata,addr,client
+
 def unpack(pack):
     length = pack[0:4]
     length = int(length)
@@ -22,7 +22,7 @@ def unpack(pack):
     de_Ticket = Des(Ticketv,Kv,1)
     Kc2v = de_Ticket[0:8]
     all_length = len(pack)
-    Authenticatorc = [4+length:4 + length + all_length - 258*3]
+    Authenticatorc = [4+length:(4 + length + all_length - 258*3)]
     de_Authenticatorc = Des(Authenticatorc,Kc2v,1)
     TS5 = de_Authenticatorc[-10:]
     C=pack[-258*3:-258*2]
@@ -30,20 +30,31 @@ def unpack(pack):
     if yanzheng(Ticketv+Authenticatorc,C,key)==True:
         print("RSA验证成功")
         packc = pack2c(Kc2v,TS5)
+        client.send(packc.encode('utf-8'))
 
 def pack2c(Kc2v,TS5):
     TS5=int(TS5)
-    pack = Des(str(TS5+1),Kc2v,0)
+    data = Des(str(TS5+1),Kc2v,0)
+    (C,d,n) = qianming(data)
+    length = str(len(data))
+    pack = "{:4}".format(length)+data+"{:258}".format(C)+"{:258}".format(d)+"{:258}".format(n)
+
     return pack
 
+def fun():
+    (indata,addr,client)=jieshou()
+    unpack(indata)
 
 
+
+def threadFunction():
+    pass
 
 
 
 
 if __name__=='__main__':
-    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    '''server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     hostname = socket.gethostname()
     ip=socket.gethostbyname(hostname)
     port = 9090
@@ -55,6 +66,31 @@ if __name__=='__main__':
         try:
             client, addr = server.accept()
             m = multiprocessing.Process(target=fun, args=(client,addr))
+
+            m.daemon = True  # daemon True设置为守护即主死子死.
+            m.start()
+        except ConnectionResetError:
+            pass
+
+        except Exception as e:
+            print(e)'''
+    '''try:
+        _thread.start_new_thread(threadFunction, ())
+    except:
+        print("Error: 无法启动线程")'''
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    hostname = socket.gethostname()
+    ip = socket.gethostbyname(hostname)
+    port = 9090
+    server.bind((ip, port))
+    server.listen(5)
+    Ktgs = '12345678'
+    Kv = '56789123'
+    while True:
+        try:
+            client, addr = server.accept()
+            m = multiprocessing.Process(target=fun, args=(client, addr))
+
             m.daemon = True  # daemon True设置为守护即主死子死.
             m.start()
         except ConnectionResetError:
